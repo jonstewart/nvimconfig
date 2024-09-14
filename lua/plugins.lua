958c03edef659d3f0db282da74697b4a1bdd4016
Jon Stewart <jstewart@strozfriedberg.com>
2024-09-14 13:47:20 -0400

    r - extract lualine to a separate plugin file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -77,20 +77,6 @@ return {
   -- },
 
 
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
